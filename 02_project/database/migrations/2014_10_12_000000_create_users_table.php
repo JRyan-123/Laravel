8638060b33efcc4071080cbe98d039cbdf55dd53
JRyan-123 <acostajoseryan4@gmail.com>
2025-07-30 11:26:49 +0800

    laravel project installed breeze

--- 02_project/database/migrations/2014_10_12_000000_create_users_table.php
+++ 02_project/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,13 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->enum('role', ['admin', 'agent', 'user'])->default('user');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
