6815f8d54bba1f48f9a03aa65b994fec2f16356f
JRyan-123 <acostajoseryan4@gmail.com>
2025-07-31 02:46:42 +0800

    add database migrations, fakes, seeders

--- 02_project/database/seeders/UsersTableSeeder.php
+++ 02_project/database/seeders/UsersTableSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use DB;
+use Illuminate\Support\Facades\Hash;
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -14,6 +15,35 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            //admin
+            [
+                'name'      =>  'Admin',
+                'username'  =>  'admin',
+                'email'     =>  'admin@gmail.com',
+                'password'  =>  Hash::make('111'),
+                'role'      =>  'admin',
+                'status'    =>  'active',
+            ],
+            //agent
+            [
+                'name'      =>  'Agent',
+                'username'  =>  'agent',
+                'email'     =>  'agent@gmail.com',
+                'password'  =>  Hash::make('111'),
+                'role'      =>  'agent',
+                'status'    =>  'active',
+            ],
+            //user
+            [
+                'name'      =>  'User',
+                'username'  =>  'user',
+                'email'     =>  'user@gmail.com',
+                'password'  =>  Hash::make('111'),
+                'role'      =>  'user',
+                'status'    =>  'active',
+            ]
+
+        ]);
     }
 }
