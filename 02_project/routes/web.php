42485525951a14dbd1d57107b19d8cc06d2c712d
JRyan-123 <acostajoseryan4@gmail.com>
2025-08-01 15:35:09 +0800

    admin login UI

--- 02_project/routes/web.php
+++ 02_project/routes/web.php
@@ -40,3 +40,4 @@ Route::middleware(['auth','role:agent'])->group(function() {
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 });
 
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
