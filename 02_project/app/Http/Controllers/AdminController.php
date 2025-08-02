a2dc345c2acf976e41a86b238f4542d86efe9371
JRyan-123 <acostajoseryan4@gmail.com>
2025-08-02 16:03:57 +0800

    add change password feature

--- 02_project/app/Http/Controllers/AdminController.php
+++ 02_project/app/Http/Controllers/AdminController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
     public function AdminDashboard() {
-          $id = Auth::user()->id;
+        $id = Auth::user()->id;
         $profileData = User::find($id);
         return view('admin.index', compact('profileData'));
     }
@@ -71,5 +72,48 @@ class AdminController extends Controller
         );
 
         return redirect()->back()->with($notification);
+    }
+    // End Method
+
+
+    public function AdminChangePassword()
+    {   
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_change_password', compact('profileData'));
+    }
+    // End Method
+
+
+    public function AdminUpdatePassword(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+        $user = Auth::user();
+        if (!Hash::check($request->old_password, $user->password)) {
+             $notification = array(
+            'message' => 'password incorrect',
+            'alert-type' => 'error'
+                );
+
+             return back()->with($notification);
+        }
+        // shortcut
+        // User::whereId(auth()->user()->id)->update([
+        //     'password' => Hash::make($request->new_password)
+        // ]);
+
+        
+        $user->password = Hash::make($request->new_password);
+        $user->save();
+
+         $notification = array(
+            'message' => 'Password updated',
+            'alert-type' => 'success'
+        );
+
+        return back()->with($notification);
     }
 }
