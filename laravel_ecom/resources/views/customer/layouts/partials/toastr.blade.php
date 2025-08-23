0e03a5c622548dd08097a1ab837879f9581e68e5
JRyan-123 <acostajoseryan4@gmail.com>
2025-08-23 23:11:40 +0800

    fix error message

--- laravel_ecom/resources/views/customer/layouts/partials/toastr.blade.php
+++ laravel_ecom/resources/views/customer/layouts/partials/toastr.blade.php
@@ -1,4 +1,10 @@
 <script>
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            toastr.error("{{ $error }}");
+        @endforeach
+    @endif
+    
     @if (session('success'))
         toastr.success("{{ session('success') }}");
     @endif
--- laravel_ecom/resources/views/vendor/layouts/partials/toastr.blade.php
+++ laravel_ecom/resources/views/vendor/layouts/partials/toastr.blade.php
@@ -1,4 +1,9 @@
 <script>
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            toastr.error("{{ $error }}");
+        @endforeach
+    @endif
     @if (session('success'))
         toastr.success("{{ session('success') }}");
     @endif
